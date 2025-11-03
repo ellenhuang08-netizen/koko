9423800f202422301a9a7ce4f1d09acce20ae44d
Ellen Huang <ellenhuang08@gmail.com>
2025-11-03 17:49:25 +0800

    animation practice(using Lottie SPM)

---
+++ koko/View/Money/MoneyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoneyViewController.swift
+//  koko
+//
+//  Created by 綸綸 on 2025/11/3.
+//
+
+import UIKit
+
+class MoneyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
